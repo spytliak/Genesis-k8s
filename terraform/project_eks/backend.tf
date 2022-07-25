a689d2dc1fbe636b3656dd4c88627530da7cf09a
Serhii Pytliak <pytliak.serhii@gmail.com>
2022-07-25 22:34:41 +0300

    added backend

--- terraform/project_eks/backend.tf
+++ terraform/project_eks/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate-project-genesis-eks"
+    bucket = "tfstate-project-genesis-eks-sp"
     key    = "genesis/eks.tfstate"
     region = "us-east-1"
   }
